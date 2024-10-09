0603d255124425def39d968158e6fa27280deeed
Felipe Hidequel <felipehidequel@gmail.com>
2024-10-08 22:32:45 -0300

    rotas e model refatoradas

--- db/models/consulta.py
+++ db/models/consulta.py
@@ -1,10 +1,12 @@
-from peewee import Model, DateField, TimeField, ForeignKeyField
+from peewee import DateField, TimeField, ForeignKeyField, CharField, BooleanField
 
 from db.db import BaseModel
 from db.models.paciente import Paciente
 
 class Consulta(BaseModel):
+    status = CharField() # "agendada", "realizada", "cancelada"
     data = DateField()
     inicio = TimeField()
     fim = TimeField()
+    presenca = BooleanField() # "presente", "ausente"
     paciente = ForeignKeyField(Paciente, backref='consultas')
